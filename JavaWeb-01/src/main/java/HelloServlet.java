30e6cbb594c106baa166956671a867a5e660489c
huanghudong <huanghudong@nstc.com.cn>
2021-04-23 17:05:50 +0800

    JavaWeb:HelloServlet

---
+++ JavaWeb-01/src/main/java/HelloServlet.java
@@ -0,0 +1,34 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author huanghudong
+ * @ClassName HelloServlet.java
+ * @Description TODO
+ * @createTime 2021年04月23日 15:02:00
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("gbk");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>你好 javaWeb!</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("<h1>Hello World!</h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GITIDEATest
 在IDEA上测试Git的使用
+
+#在IDEA中配置Tomcat
+必须要配置默认的artifacts，tomcat被idea接管后不会去本地的webapps
+文件夹下找应用，而是直接在项目的out目录下启动应用。这就是为什么没有tomcat默认
+欢迎页面的原因。
+
+> target是java源码编译目录，out是项目发布目录
